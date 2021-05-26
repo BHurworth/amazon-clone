2148273690204c8d6423ad9c6c260b746e06a6ca
“BHurworth” <ben.hurworth@gmail.com>
2021-05-26 15:35:03 +0100

    feat: useEffect to manageonLoad loading animation/errors

--- amazon_cloned/frontend/src/components/screens/HomeScreen.js
+++ amazon_cloned/frontend/src/components/screens/HomeScreen.js
@@ -1,15 +1,40 @@
-import React from 'react'
-import data from '../../data';
-import Product from '../../components/Product';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Product from "../../components/Product";
+import LoadingBox from "../LoadingBox";
+import MessageBox from "../MessageBox";
 
 export default function HomeScreen() {
-    return (
-        <div>
-          <div className="row center">
-            {data.products.map((product) => (
-             <Product product={product} key={product._id}></Product>
-            ))}
-          </div>
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get("/api/products");
+        setProducts(data);
+        setLoading(false);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <div className="row center">
+          {products.map((product) => (
+            <Product product={product} key={product._id}></Product>
+          ))}
         </div>
-    )
+      )}
+    </div>
+  );
 }
